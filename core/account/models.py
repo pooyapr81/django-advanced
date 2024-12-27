3b5fcc2a62ad4fcf8919bfd11f220c1340c8a8a9
pooya <Pooya.parva81@gmail.com>
2024-12-27 14:42:55 +0330

    write account models.py (2)
    create manager for user

--- core/account/models.py
+++ core/account/models.py
@@ -2,6 +2,39 @@ from django.db import models
 from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser, PermissionsMixin)
 
 
+class UserManager(BaseUserManager):
+    """
+    custom user model manager where email is the unique identifiers
+    for authentication instead of usernames.
+    """
+
+    def create_user(self, email, password, **extra_fields):
+        """
+        create and save a user with the given email and password and extra data
+        """
+        if not email:
+            raise ValueError(-("the email must be set"))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        create and save a superuser with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(-('superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(-('superuser must have is_superuser=True.'))
+        return self.create_user(email, password, **extra_fields)
+
+
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     is_staff = models.BooleanField(default=False)
@@ -12,6 +45,8 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+
+    objects = UserManager()
 
     def __str__(self):
         return self.email
