3a59cb7081b3b9720d4f2e8f5e31f0df60505e63
pooya <Pooya.parva81@gmail.com>
2024-12-28 15:42:10 +0330

    create admin.py for user

--- core/account/admin.py
+++ core/account/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('email', 'is_superuser', 'is_active')
+    list_filter = ('email', 'is_superuser', 'is_active')
+    search_fields = ('email',)
+    ordering = ('email',)
+
+
+admin.site.register(User, CustomUserAdmin)
